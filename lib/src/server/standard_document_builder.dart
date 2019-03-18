0a6ca45ab027f3b6ac587f893bc5c19d75f1aec9
Alexey Karapetov <karapetov@gmail.com>
2019-03-17 20:46:12 -0700

    formatting

--- lib/src/server/standard_document_builder.dart
+++ lib/src/server/standard_document_builder.dart
@@ -12,91 +12,95 @@ class StandardDocumentBuilder implements DocumentBuilder {
 
   Document error(Iterable<JsonApiError> errors) => Document.error(errors);
 
-  Document<ResourceCollectionData> collection(Iterable<Resource> resource,
-      String type, Uri self,
-      {Page page, Iterable<Resource> included}) =>
-      Document(ResourceCollectionData(resource.map(
-          _ResourceObject),
-          self: Link(self),
-          pagination: page == null
-              ? Pagination.empty()
-              : Pagination.fromLinks(page.map((_) =>
-              Link(
-                  uriBuilder.collection(type, parameters: _.parameters))))));
+  Document<ResourceCollectionData> collection(
+      Iterable<Resource> resource, String type, Uri self,
+      {Page page, Iterable<Resource> included}) {
+    return Document(ResourceCollectionData(resource.map(_resourceObject),
+        self: Link(self),
+        pagination: page == null
+            ? Pagination.empty()
+            : Pagination.fromLinks(page.map((_) =>
+                Link(uriBuilder.collection(type, parameters: _.parameters))))));
+  }
 
   Document<ResourceCollectionData> relatedCollection(
-      Iterable<Resource> resource, String type,
-      String id, String relationship, Uri self,
-      {Page page, Iterable<Resource> included}) =>
-      Document(ResourceCollectionData(resource.map(_ResourceObject),
-          self: Link(self),
-          pagination: page == null
-              ? Pagination.empty()
-              : Pagination.fromLinks(page.map((_) =>
-              Link(
-                  uriBuilder.related(
-                      type, id, relationship, parameters: _.parameters))))));
-
-  Document<ResourceData> resource(Resource resource, String type, String id,
-      Uri self,
-      {Iterable<Resource> included}) =>
-      Document(
-        ResourceData(_ResourceObject(resource),
-            self: Link(uriBuilder.resource(type, id)),
-            included: included?.map(_ResourceObject)),
-      );
-
-  Document<ResourceData> relatedResource(Resource resource, String type,
+      Iterable<Resource> resource,
+      String type,
       String id,
-      String relationship, Uri self,
-      {Iterable<Resource> included}) =>
-      Document(
-        ResourceData(
-            _ResourceObject(resource), included: included?.map(_ResourceObject),
-            self: Link(uriBuilder.related(type, id, relationship))),
-      );
+      String relationship,
+      Uri self,
+      {Page page,
+      Iterable<Resource> included}) {
+    final pagination = _pagination(page, type, id, relationship);
+    return Document(ResourceCollectionData(resource.map(_resourceObject),
+        self: Link(self), pagination: pagination));
+  }
+
+  Document<ResourceData> resource(
+      Resource resource, String type, String id, Uri self,
+      {Iterable<Resource> included}) {
+    return Document(
+      ResourceData(_resourceObject(resource),
+          self: Link(uriBuilder.resource(type, id)),
+          included: included?.map(_resourceObject)),
+    );
+  }
+
+  Document<ResourceData> relatedResource(
+      Resource resource, String type, String id, String relationship, Uri self,
+      {Iterable<Resource> included}) {
+    return Document(
+      ResourceData(_resourceObject(resource),
+          included: included?.map(_resourceObject),
+          self: Link(uriBuilder.related(type, id, relationship))),
+    );
+  }
 
   Document<ToMany> toMany(Iterable<Identifier> collection, String type,
-      String id,
-      String relationship, Uri self) =>
-      Document(ToMany(
-          collection.map(_IdentifierObject),
-          self: Link(uriBuilder.relationship(type, id, relationship)),
-          related: Link(uriBuilder.related(type, id, relationship))));
+      String id, String relationship, Uri self) {
+    return Document(ToMany(collection.map(_rdentifierObject),
+        self: Link(uriBuilder.relationship(type, id, relationship)),
+        related: Link(uriBuilder.related(type, id, relationship))));
+  }
 
   Document<ToOne> toOne(Identifier identifier, String type, String id,
-      String relationship, Uri self) =>
-      Document(ToOne(
-          nullable(_IdentifierObject)(identifier),
-          self: Link(uriBuilder.relationship(type, id, relationship)),
-          related: Link(uriBuilder.related(type, id, relationship))));
+      String relationship, Uri self) {
+    return Document(ToOne(nullable(_rdentifierObject)(identifier),
+        self: Link(uriBuilder.relationship(type, id, relationship)),
+        related: Link(uriBuilder.related(type, id, relationship))));
+  }
 
   Document meta(Map<String, Object> meta) => Document.empty(meta);
 
-  IdentifierObject _IdentifierObject(Identifier id) =>
+  IdentifierObject _rdentifierObject(Identifier id) =>
       IdentifierObject(id.type, id.id);
 
-  ResourceObject _ResourceObject(Resource resource) {
-    final relationships =
-    <String, Relationship>{};
-    relationships.addAll(resource.toOne.map((k, v) =>
-        MapEntry(
-            k, ToOne(nullable(_IdentifierObject)(v), self: Link(
-            uriBuilder.relationship(resource.type, resource.id, k)),
-            related: Link(
-                uriBuilder.related(resource.type, resource.id, k))))));
-    relationships.addAll(
-        resource.toMany.map(
-                (k, v) =>
-                MapEntry(
-                    k, ToMany(v.map(_IdentifierObject), self: Link(
-                    uriBuilder.relationship(resource.type, resource.id, k)),
-                    related: Link(
-                        uriBuilder.related(resource.type, resource.id, k))))));
+  ResourceObject _resourceObject(Resource resource) {
+    final relationships = <String, Relationship>{};
+    relationships.addAll(resource.toOne.map((k, v) => MapEntry(
+        k,
+        ToOne(nullable(_rdentifierObject)(v),
+            self: Link(uriBuilder.relationship(resource.type, resource.id, k)),
+            related:
+                Link(uriBuilder.related(resource.type, resource.id, k))))));
+    relationships.addAll(resource.toMany.map((k, v) => MapEntry(
+        k,
+        ToMany(v.map(_rdentifierObject),
+            self: Link(uriBuilder.relationship(resource.type, resource.id, k)),
+            related:
+                Link(uriBuilder.related(resource.type, resource.id, k))))));
 
     return ResourceObject(resource.type, resource.id,
         attributes: resource.attributes,
         relationships: relationships,
         self: Link(uriBuilder.resource(resource.type, resource.id)));
+  }
+
+  Pagination _pagination(
+      Page page, String type, String id, String relationship) {
+    return page == null
+        ? Pagination.empty()
+        : Pagination.fromLinks(page.map((_) => Link(uriBuilder
+            .related(type, id, relationship, parameters: _.parameters))));
   }
 }
