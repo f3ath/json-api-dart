3b861eb8bd787574b2ef569d310fbb1049885e80
Alexey Karapetov <karapetov@gmail.com>
2019-04-18 20:36:46 -0700

    Allow const ctor

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:json_api/src/nullable.dart';
 import 'package:json_api/src/response.dart';
 import 'package:json_api/src/status_code.dart';
-import 'package:json_api/src/nullable.dart';
 import 'package:json_api_document/json_api_document.dart';
 import 'package:json_api_document/parser.dart';
 
@@ -16,14 +16,14 @@ typedef http.Client HttpClientFactory();
 class JsonApiClient {
   static const contentType = 'application/vnd.api+json';
 
-  JsonApiParser _parser = const JsonApiParser();
+  final JsonApiParser _parser = const JsonApiParser();
 
   final HttpClientFactory _factory;
 
   /// JSON:API client uses Dart's native Http Client internally.
   /// To customize its behavior you can pass the [factory] function.
-  JsonApiClient({HttpClientFactory factory})
-      : _factory = factory ?? (() => http.Client());
+  const JsonApiClient({HttpClientFactory factory})
+      : _factory = factory ?? (_defaultFactory);
 
   /// Fetches a resource collection by sending a GET request to the [uri].
   /// Use [headers] to pass extra HTTP headers.
@@ -194,3 +194,5 @@ class JsonApiClient {
     }
   }
 }
+
+http.Client _defaultFactory() => http.Client();
