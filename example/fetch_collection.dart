56d9145f531e5fa8f23aa8d5851f8ce0ef77c374
f3ath <karapetov@gmail.com>
2020-01-01 14:10:23 -0800

    Fix #74

--- example/fetch_collection.dart
+++ example/fetch_collection.dart
@@ -1,11 +1,11 @@
 import 'package:http/http.dart';
 import 'package:json_api/client.dart';
 
-/// Start the `cars_server.dart` first!
+/// Start `dart example/cars_server.dart` first
 void main() async {
   final httpClient = Client();
   final jsonApiClient = JsonApiClient(httpClient);
-  final url = Uri.parse('http://localhost:8080/companies/2');
+  final url = Uri.parse('http://localhost:8080/companies');
   final response = await jsonApiClient.fetchCollection(url);
   httpClient.close(); // Don't forget to close the http client
   print('The collection page size is ${response.data.collection.length}');
