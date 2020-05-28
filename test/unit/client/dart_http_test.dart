a9af339a899e1ce3cc190b3f3e06fa251b9cc88e
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-05-28 14:52:04 +0300

    refactor

--- test/unit/client/dart_http_test.dart
+++ test/unit/client/dart_http_test.dart
@@ -4,25 +4,20 @@ import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
 import 'package:json_api/client.dart';
 import 'package:json_api/http.dart';
-import 'package:json_api/routing.dart';
-import 'package:json_api/server.dart';
-import 'package:json_api/src/document/resource.dart';
 import 'package:test/test.dart';
 
-import '../../helper/test_http_handler.dart';
-
 void main() {
-  group("Decode body with", () {
+  group('Decode body with', () {
     final stringBodyRu = 'йцукен';
     final bytesBodyRu = utf8.encode(stringBodyRu);
     final stringBodyEn = 'qwerty';
     final bytesBodyEn = utf8.encode(stringBodyEn);
 
-    final dartHttpWithBytesBodyAndEncoding = (
+    final buildResponse = (
       List<int> bytesBody,
       Encoding encoding,
-    ) {
-      return DartHttp(
+    ) async {
+      final dartHttp = DartHttp(
         MockClient(
           (request) async {
             return http.Response.bytes(bytesBody, 200);
@@ -30,41 +25,27 @@ void main() {
         ),
         encoding,
       );
+
+      return dartHttp.call(HttpRequest('', Uri.parse('http://test.com')));
     };
 
     test('UTF-8 ru', () async {
-      final dartHttp = dartHttpWithBytesBodyAndEncoding(bytesBodyRu, utf8);
-
-      final response =
-          await dartHttp.call(HttpRequest('', Uri.parse('http://test.com')));
-
+      final response = await buildResponse(bytesBodyRu, utf8);
       expect(response.body, equals(stringBodyRu));
     });
 
     test('latin1 ru', () async {
-      final dartHttp = dartHttpWithBytesBodyAndEncoding(bytesBodyRu, latin1);
-
-      final response =
-          await dartHttp.call(HttpRequest('', Uri.parse('http://test.com')));
-
+      final response = await buildResponse(bytesBodyRu, latin1);
       expect(response.body, isNot(equals(stringBodyRu)));
     });
 
     test('UTF-8 en', () async {
-      final dartHttp = dartHttpWithBytesBodyAndEncoding(bytesBodyEn, utf8);
-
-      final response =
-          await dartHttp.call(HttpRequest('', Uri.parse('http://test.com')));
-
+      final response = await buildResponse(bytesBodyEn, utf8);
       expect(response.body, equals(stringBodyEn));
     });
 
     test('latin1 en', () async {
-      final dartHttp = dartHttpWithBytesBodyAndEncoding(bytesBodyEn, latin1);
-
-      final response =
-          await dartHttp.call(HttpRequest('', Uri.parse('http://test.com')));
-
+      final response = await buildResponse(bytesBodyEn, latin1);
       expect(response.body, equals(stringBodyEn));
     });
   });
