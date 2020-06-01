9426bff7f5ff203f59af0eb09440d161f280c432
Kevin Martin <knmartinm@gmail.com>
2020-06-01 13:56:19 -0500

    Added filtering support for collections (#97)

--- lib/query.dart
+++ lib/query.dart
@@ -1,6 +1,7 @@
 library query;
 
 export 'package:json_api/src/query/fields.dart';
+export 'package:json_api/src/query/filter.dart';
 export 'package:json_api/src/query/include.dart';
 export 'package:json_api/src/query/page.dart';
 export 'package:json_api/src/query/query_parameters.dart';
