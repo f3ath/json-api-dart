9ae58bd94f2e66880fbf9b5dd445be10ce07a194
dan12411 <dan12411@ecrowdmedia.com>
2020-07-30 14:51:31 +0800

    use TestHttpHandler to check request actually has the meta property

--- test/unit/client/creating_resource_with_meta_test.dart
+++ test/unit/client/creating_resource_with_meta_test.dart
@@ -1,49 +1,28 @@
 import 'package:json_api/client.dart';
 import 'package:json_api/document.dart';
-import 'package:json_api/routing.dart';
-import 'package:json_api/server.dart';
+import 'package:json_api/http.dart';
 import 'package:test/test.dart';
 
-import '../../helper/expect_resources_equal.dart';
+import '../../helper/test_http_handler.dart';
 
 void main() async {
-  final host = 'localhost';
-  final port = 80;
-  final base = Uri(scheme: 'http', host: host, port: port);
-  final routing = StandardRouting(base);
+  test('request actually has the meta property', () async {
+    final handler = TestHttpHandler();
+    final client = JsonApiClient(handler);
 
-  group('Client-generated ID', () {
-    JsonApiClient client;
-    RoutingClient routingClient;
+    final uri = Uri.parse('https://github.com/f3ath/json-api-dart');
+    final person =
+        Resource('people', '123', attributes: {'name': 'Te Cheng Hung'});
+    final meta = {'friend': 'Martin Fowler'};
 
-    setUp(() async {
-      final repository = InMemoryRepository({
-        'books': {},
-        'people': {},
-        'companies': {},
-        'noServerId': {},
-        'fruits': {},
-        'apples': {}
-      });
-      final server = JsonApiServer(RepositoryController(repository));
-      client = JsonApiClient(server);
-      routingClient = RoutingClient(client, routing);
-    });
+    handler.nextResponse = HttpResponse(500, body: 'Something went wrong');
+    await client.createResourceAt(uri, person, meta: meta);
 
-    test('204 No Content', () async {
-      final person =
-          Resource('people', '123', attributes: {'name': 'Martin Fowler'});
-      final r = await client.createResourceAt(
-          routing.collection('people'), person,
-          meta: {'456': 'friends'});
-      expect(r.isSuccessful, isTrue);
-      expect(r.statusCode, 204);
-      expect(r.location, isNull);
-      expect(r.data, isNull);
-      final r1 = await routingClient.fetchResource(person.type, person.id);
-      expect(r1.isSuccessful, isTrue);
-      expect(r1.statusCode, 200);
-      expectResourcesEqual(r1.data.unwrap(), person);
-    });
+    final request = handler.requestLog.first;
+    expect(request, isNotNull);
+    expect(request.uri, uri);
+    expect(request.method, 'POST');
+    expect(request.body,
+        '{"data":{"type":"people","id":"123","attributes":{"name":"Te Cheng Hung"}},"meta":{"friend":"Martin Fowler"}}');
   });
 }
