3c54ccd90888f0dcd7ea845ac9709e0e1b7b78ac
f3ath <karapetov@gmail.com>
2022-09-10 19:50:49 -0700

    Allow repeating identifiers in ToMany

--- lib/src/document/many.dart
+++ lib/src/document/many.dart
@@ -5,23 +5,23 @@ import 'package:json_api/src/document/resource_collection.dart';
 
 class ToMany extends Relationship {
   ToMany(Iterable<Identifier> identifiers) {
-    for (var id in identifiers) {
-      _map[id.key] = id;
-    }
+    _ids.addAll(identifiers);
   }
 
-  final _map = <String, Identifier>{};
+  final _ids = <Identifier>[];
 
   @override
-  Map<String, Object> toJson() =>
-      {'data': _map.values.toList(), ...super.toJson()};
+  Map<String, Object> toJson() => {
+        'data': [..._ids],
+        ...super.toJson()
+      };
 
   @override
-  Iterator<Identifier> get iterator => _map.values.iterator;
+  Iterator<Identifier> get iterator => _ids.iterator;
 
   /// Finds the referenced elements which are found in the [collection].
   /// The resulting [Iterable] may contain fewer elements than referred by the
   /// relationship if the [collection] does not have all of them.
-  Iterable<Resource> findIn(ResourceCollection collection) =>
-      _map.keys.map((key) => collection[key]).whereType();
+  Iterable<Resource> findIn(ResourceCollection collection) => collection.where(
+      (resource) => any((identifier) => identifier.identifies(resource)));
 }
