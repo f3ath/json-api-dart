7287f758a38a5f4fb7fca0d0dd9958cbf0d8e81e
f3ath <karapetov@gmail.com>
2022-12-29 22:03:10 -0800

    Do not set empty body. Fixes #129

--- lib/src/client/persistent_handler.dart
+++ lib/src/client/persistent_handler.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-import 'package:http_parser/http_parser.dart';
 import 'package:json_api/http.dart';
+import 'package:json_api/src/client/message_converter.dart';
 
 /// Handler which relies on the built-in Dart HTTP client.
 /// It is the developer's responsibility to instantiate the client and
@@ -10,46 +10,26 @@ import 'package:json_api/http.dart';
 class PersistentHandler implements HttpHandler {
   /// Creates a new instance of the handler. Do not forget to call `close()` on
   /// the [client] when it's not longer needed.
-  PersistentHandler(this.client, {this.defaultEncoding = utf8});
+  ///
+  /// Use [messageConverter] to fine tune the HTTP request/response conversion.
+  PersistentHandler(
+      this.client,
+      {@Deprecated('Deprecated in favor of MessageConverter.'
+          ' To be removed in version 6.0.0')
+          this.defaultEncoding = utf8,
+      MessageConverter? messageConverter})
+      : _converter = messageConverter ??
+            MessageConverter(defaultResponseEncoding: defaultEncoding);
 
   final Client client;
   final Encoding defaultEncoding;
+  final MessageConverter _converter;
 
   @override
   Future<HttpResponse> handle(HttpRequest request) async {
-    final response = await Response.fromStream(
-        await client.send(Request(request.method, request.uri)
-          ..headers.addAll(request.headers)
-          ..body = request.body));
-    final responseBody =
-        _encodingForHeaders(response.headers).decode(response.bodyBytes);
-    return HttpResponse(response.statusCode, body: responseBody)
-      ..headers.addAll(response.headers);
-  }
-
-  /// Returns the encoding to use for a response with the given headers.
-  ///
-  /// Defaults to [defaultEncoding] if the headers don't specify a charset or if that
-  /// charset is unknown.
-  Encoding _encodingForHeaders(Map<String, String> headers) =>
-      _encodingForCharset(
-          _contentTypeForHeaders(headers).parameters['charset']);
-
-  /// Returns the [Encoding] that corresponds to [charset].
-  ///
-  /// Returns [defaultEncoding] if [charset] is null or if no [Encoding] was found that
-  /// corresponds to [charset].
-  Encoding _encodingForCharset(String? charset) {
-    if (charset == null) return defaultEncoding;
-    return Encoding.getByName(charset) ?? defaultEncoding;
-  }
-
-  /// Returns the [MediaType] object for the given headers's content-type.
-  ///
-  /// Defaults to `application/octet-stream`.
-  MediaType _contentTypeForHeaders(Map<String, String> headers) {
-    final contentType = headers['content-type'];
-    if (contentType != null) return MediaType.parse(contentType);
-    return MediaType('application', 'octet-stream');
+    final convertedRequest = _converter.request(request);
+    final streamedResponse = await client.send(convertedRequest);
+    final response = await Response.fromStream(streamedResponse);
+    return _converter.response(response);
   }
 }
