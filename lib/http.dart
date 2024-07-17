ae8fb36a46f90887083b05a9f47930cfd27b91e3
f3ath <karapetov@gmail.com>
2024-07-16 18:56:07 -0700

    Format

--- lib/http.dart
+++ lib/http.dart
@@ -1,4 +1,5 @@
 /// Common HTTP utilities for JSON:API clients and servers.
 /// WARNING: This library is in beta stage. The API is subject to change.
 library http;
+
 export 'package:json_api/src/http/status_code.dart';
